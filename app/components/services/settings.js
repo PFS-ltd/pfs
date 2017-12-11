7075a31dece5bb789d71e7f82fb69d51ae27afbe
Dmitriy <poberezhniy.d@gmail.com>
2017-12-11 15:32:16 +0300

    add services for firebase

---
+++ app/components/services/settings.js
@@ -0,0 +1,71 @@
+app.service('settingsService', ['$state', 'Auth', "$firebaseObject", "$firebaseArray",
+function ($state, Auth, $firebaseObject, $firebaseArray) {
+    var uId = Auth.$getAuth().uid;
+
+    var rolesRef = firebase.database().ref('users/' + uId + '/settings/roles');
+    var rolesArr = $firebaseArray(rolesRef);
+
+    var themeRef = firebase.database().ref('users/' + uId + '/settings/themeColor');
+    var themes = $firebaseArray(themeRef);
+
+
+    var userNameRef = firebase.database().ref('users/' + uId + '/settings/username');
+    var userNameRef = $firebaseObject(userNameRef);
+
+
+    return {
+        getRolesArray: function () {
+            return rolesArr;
+        },
+        addRole: function (item) {
+            rolesArr.$add(item);
+        },
+        delIRole: function (item) {
+            rolesArr.$remove(item);
+        },
+        updateRole: function (item) {
+            rolesArr.$save(item);
+        },
+        getRoleByKey: function (key) {
+            // console.log(key)
+            return rolesArr.$getRecord(key);
+        },
+
+
+
+        // getCostsTemplateArray: function () {
+        //     return costsTemplate;
+        // },
+        // addItemInCostsTemplate: function (item) {
+        //     costsTemplate.$add(item);
+        // },
+        // delItemInCostsTemplate: function (item) {
+        //     costsTemplate.$remove(item);
+        // },
+        // updateItemInCostsTemplate: function (item) {
+        //     costsTemplate.$save(item);
+        // },
+        // getItemInCostsTemplateByKey: function (key) {
+        //     return costsTemplate.$getRecord(key);
+        // },
+
+
+        // getCostsTransferArray: function () {
+        //     return costsTransfers;
+        // },
+
+        // addItemInCostsTransfer: function (item) {
+        //     costsCategories.$add(item);
+        // },
+        // delItemInCostsTransfer: function (item) {
+        //     costsCategories.$remove(item);
+        // },
+        // updateItemInCostsTransfer: function (item) {
+        //     costsCategories.$save(item);
+        // },
+        // getItemInCostsTransferByKey: function (key) {
+        //    return costsCategories.$getRecord(key);
+        // },
+
+    };
+}]);
